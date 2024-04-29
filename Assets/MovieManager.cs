4b2e47e1087aab6800bf67256148e5673a2d1e0e
Maxwell Gonsalves <52668552+mag2352@users.noreply.github.com>
2024-04-29 11:11:42 -0400

    attempt at resolving tile issues

--- Assets/MovieManager.cs
+++ Assets/MovieManager.cs
@@ -1,144 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VRTemplate;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class MovieManager : MonoBehaviour
 {
-    //Classics
-    public void breakFastatTiffanys() {
-        //Load bkfast at tiffanys movie. 
+    public VideoPlayer videoPlayer;
+    public List<VideoClip> videoClips;
+    public GameObject videoPlayerCanvas;
+    public void PlayVideo(int index)
+    {
+        if (index >= 0 && index < videoClips.Count)
+        {
+            videoPlayerCanvas.SetActive(true);
+            videoPlayer.clip = videoClips[index];
+            videoPlayer.GetComponent<VideoTimeScrubControl>().VideoStop();
+            Debug.Log("Loading video: " + videoClips[index].name);
+        }
+        else
+        {
+            Debug.LogError("Index out of range");
+        }
     }
-
-    public void twelveAngryMen() {
-        //Load twelve angry men
-    }
-    
-    public void prideAndPrejudice() {
-        //load pride and prejudice movie
-    }
-
-    public void rambo() {
-        //load rambo
-    }
-
-    //Comedy
-    public void genie() {
-        //Load genie
-    }
-
-    public void outLaws() {
-        //Load outlaws
-    }
-    
-    public void quizLady() {
-        //load  quizLady
-    }
-
-    public void rickyStanicky() {
-        //load ricky stanicky
-    }
-
-
-    //Comedy
-    public void it() {
-        //Load It
-    }
-
-    public void smile() {
-        //Load Smile
-    }
-    
-    public void theRing() {
-        //load  The ring
-    }
-
-    public void theExorcist() {
-        //load the exorcist
-    }
-
-    //International
-    public void yehJawaaniHaiDeewani(){
-        //Load yehJawaaniHaiDeewani
-    }
-
-    public void queenOfTears() {
-        //Load QueenOfTears
-    }
-    
-    public void theEmpress() {
-        //load  TheEmpress
-    }
-
-    public void dilDhadakneDo() {
-        //load DilDhadakneDo
-    }
-
-    //Kids
-    public void toyStory(){
-        //Load toy story
-    }
-
-    public void moana() {
-        //Load moana
-    }
-    
-    public void plum() {
-        //load  plum
-    }
-
-    public void minions() {
-        //load minions
-    }
-
-    //Mystery
-    public void goneGirl(){
-        //Load gone girl
-    }
-
-    public void knivesOut() {
-        //Load knives out
-    }
-    
-    public void prisoners() {
-        //load  prisoners
-    }
-
-    public void theGirlOnTheTrain() {
-        //load the girl on the train
-    }
-
-    //Romance
-    public void titanic(){
-        //Load Titanic
-    }
-
-    public void laLaLand() {
-        //Load La La Land
-    }
-    
-    public void theProposal() {
-        //load  The Proposal
-    }
-
-    public void aboutTime() {
-        //load About Time
-    }
-
-    //Scifi
-    public void avatar(){
-        //Load Titanic
-    }
-
-    public void matrix() {
-        //Load La La Land
-    }
-    
-    public void jurassicWorld() {
-        //load  The Proposal
-    }
-
-    public void interstellar() {
-        //load About Time
-    }
-
 }
