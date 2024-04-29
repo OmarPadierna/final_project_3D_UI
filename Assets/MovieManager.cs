1178f3f3cefa05b22b74f80c9ada15e44dd7754f
Omar Padierna <omar.ps16@gmail.com>
2024-04-28 20:21:58 -0400

    Adds interactable tiles and gifs

---
+++ Assets/MovieManager.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovieManager : MonoBehaviour
+{
+    //Classics
+    public void breakFastatTiffanys() {
+        //Load bkfast at tiffanys movie. 
+    }
+
+    public void twelveAngryMen() {
+        //Load twelve angry men
+    }
+    
+    public void prideAndPrejudice() {
+        //load pride and prejudice movie
+    }
+
+    public void rambo() {
+        //load rambo
+    }
+
+    //Comedy
+    public void genie() {
+        //Load genie
+    }
+
+    public void outLaws() {
+        //Load outlaws
+    }
+    
+    public void quizLady() {
+        //load  quizLady
+    }
+
+    public void rickyStanicky() {
+        //load ricky stanicky
+    }
+
+
+    //Comedy
+    public void it() {
+        //Load It
+    }
+
+    public void smile() {
+        //Load Smile
+    }
+    
+    public void theRing() {
+        //load  The ring
+    }
+
+    public void theExorcist() {
+        //load the exorcist
+    }
+
+    //International
+    public void yehJawaaniHaiDeewani(){
+        //Load yehJawaaniHaiDeewani
+    }
+
+    public void queenOfTears() {
+        //Load QueenOfTears
+    }
+    
+    public void theEmpress() {
+        //load  TheEmpress
+    }
+
+    public void dilDhadakneDo() {
+        //load DilDhadakneDo
+    }
+
+    //Kids
+    public void toyStory(){
+        //Load toy story
+    }
+
+    public void moana() {
+        //Load moana
+    }
+    
+    public void plum() {
+        //load  plum
+    }
+
+    public void minions() {
+        //load minions
+    }
+
+    //Mystery
+    public void goneGirl(){
+        //Load gone girl
+    }
+
+    public void knivesOut() {
+        //Load knives out
+    }
+    
+    public void prisoners() {
+        //load  prisoners
+    }
+
+    public void theGirlOnTheTrain() {
+        //load the girl on the train
+    }
+
+    //Romance
+    public void titanic(){
+        //Load Titanic
+    }
+
+    public void laLaLand() {
+        //Load La La Land
+    }
+    
+    public void theProposal() {
+        //load  The Proposal
+    }
+
+    public void aboutTime() {
+        //load About Time
+    }
+
+    //Scifi
+    public void avatar(){
+        //Load Titanic
+    }
+
+    public void matrix() {
+        //Load La La Land
+    }
+    
+    public void jurassicWorld() {
+        //load  The Proposal
+    }
+
+    public void interstellar() {
+        //load About Time
+    }
+
+}
